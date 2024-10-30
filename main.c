8f3695430a297494b0fe9fa2bbaf0d1d0551aa43
Anish <anishg24@gmail.com>
2024-10-29 20:32:26 -0700

    temp commit

--- main.c
+++ main.c
@@ -5,13 +5,20 @@
 
 #define BUFF_SIZE (1<<21)
 #define SENSITIVITY 40
-#define SAMPLES 1000
 #define LINE_SIZE 64
+#define SECRET_SIZE 100
+#define BOUND 20
 
 //** Write your victim function here */
 // Assume secret_array[47] is your secret value
 // Assume the bounds check bypass prevents you from loading values above 20
 // Use a secondary load instruction (as shown in pseudo-code) to convert secret value to address
+
+// void victim_function(int idx) {
+//   int tmp;
+//   if (idx < BOUND)
+//     tmp = dummy_array[secret_array[idx*LINE_SIZE] * LINE_SIZE];
+// }
 
 int main(int argc, char **argv)
 {
@@ -33,32 +40,42 @@ int main(int argc, char **argv)
 
     //** STEP 1: Allocate an array into the mmap */
     int *secret_array = (int *)huge_page;
+    int *dummy_array = (int *)huge_page;
 
     // Initialize the array
-    for (int i = 0; i < 100*LINE_SIZE; i += LINE_SIZE) {
-      secret_array[i] = i;
+    for (int i = 0; i < SECRET_SIZE*LINE_SIZE; i += LINE_SIZE) {
+      secret_array[i] = i/LINE_SIZE;
     }
 
     //** STEP 2: Mistrain the branch predictor by calling victim function here */
     // To prevent any kind of patterns, ensure each time you train it a different number of times
 
-
-    //** STEP 3: Clear cache using clflsuh from utility.h */
-    for (int a = 0; a < 100*LINE_SIZE; a += LINE_SIZE) {
-        clflush(&secret_array[a]);
-    }
+    // for (int i = 0; i < BOUND; i++) {
+    //   victim_function(i); 
+    // } 
+    //
+    // lfence();
+    //
+    // //** STEP 3: Clear cache using clflush from utility.h */
+    // for (int a = 0; a < SECRET_SIZE*LINE_SIZE; a += LINE_SIZE) {
+    //   clflush(&secret_array[a]);
+    // }
 
     //** STEP 4: Call victim function again with bounds bypass value */
     int message = 47;
-    secret_array[message*LINE_SIZE] = 1;
+    secret_array[message * LINE_SIZE] = 1;
+    // victim_function(message);
+    // clflush(&message);
 
     //** STEP 5: Reload mmap to see load times */
     // Just read the mmap's first 100 integers
-    for (int j = 0; j < 100; j++) {
+    for (int j = 0; j < SECRET_SIZE; j++) {
       if (measure_one_block_access_time((uint64_t)(secret_array + (j*LINE_SIZE))) < SENSITIVITY) {
-        printf("Line is %d\n", j);
+        printf("Line is %d.\n", j);
       }
     }
+
+    printf("Done.\n");
 
     return 0;
 }
